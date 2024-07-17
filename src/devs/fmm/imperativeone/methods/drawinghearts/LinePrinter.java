22797907d79ca018ff63e8eb0fa0d5aa43aec552
fmmdevs <fmm.devs@gmail.com>
2024-07-18 00:12:25 +0200

    Implement Overloaded Line Methods

--- src/devs/fmm/imperativeone/methods/drawinghearts/LinePrinter.java
+++ src/devs/fmm/imperativeone/methods/drawinghearts/LinePrinter.java
@@ -1,7 +1,23 @@
 package devs.fmm.imperativeone.methods.drawinghearts;
 
 public class LinePrinter {
-    public static void line(){
+    public static void line() {
         System.out.println("♥♥♥♥♥♥♥♥♥♥");
+    }
+
+    public static void line(int len) {
+        line(len,'-');
+    }
+
+    public static void line(int len, char c) {
+        for (int i = 0; i < len; i++) {
+            System.out.print(c);
+        }
+    }
+
+    public static void line(String prefix, int len, char c, String suffix){
+        System.out.print(prefix);
+        line(len,c);
+        System.out.print(suffix);
     }
 }
--- src/devs/fmm/imperativeone/methods/drawinghearts/LinePrinterDemo.java
+++ src/devs/fmm/imperativeone/methods/drawinghearts/LinePrinterDemo.java
@@ -1,7 +1,19 @@
 package devs.fmm.imperativeone.methods.drawinghearts;
 
+import javax.sound.sampled.Line;
+
 public class LinePrinterDemo {
     public static void main(String[] args) {
         LinePrinter.line();
+
+        LinePrinter.line(4);
+
+        System.out.println();
+
+        LinePrinter.line(4, '$');
+
+        System.out.println();
+
+        LinePrinter.line("╠", 3, '=', "╣");
     }
 }
