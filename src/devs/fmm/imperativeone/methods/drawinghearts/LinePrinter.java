1e8a82682ee9d2070399573a8ec2dccf9c900c36
fmmdevs <fmm.devs@gmail.com>
2024-07-18 00:02:17 +0200

    drawing hearts

---
+++ src/devs/fmm/imperativeone/methods/drawinghearts/LinePrinter.java
@@ -0,0 +1,7 @@
+package devs.fmm.imperativeone.methods.drawinghearts;
+
+public class LinePrinter {
+    public static void line(){
+        System.out.println("♥♥♥♥♥♥♥♥♥♥");
+    }
+}
---
+++ src/devs/fmm/imperativeone/methods/drawinghearts/LinePrinterDemo.java
@@ -0,0 +1,7 @@
+package devs.fmm.imperativeone.methods.drawinghearts;
+
+public class LinePrinterDemo {
+    public static void main(String[] args) {
+        LinePrinter.line();
+    }
+}
