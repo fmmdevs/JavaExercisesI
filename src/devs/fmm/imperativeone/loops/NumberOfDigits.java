4c1cab820ca4c01a8900065b3190c4ab48b7aa70
fmmdevs <fmm.devs@gmail.com>
2024-07-17 21:13:41 +0200

    Get the Number of Digits of a Number

---
+++ src/devs/fmm/imperativeone/loops/NumberOfDigits.java
@@ -0,0 +1,26 @@
+package devs.fmm.imperativeone.loops;
+
+public class NumberOfDigits {
+    public static void main(String[] args) {
+
+        System.out.println("Enter a number");
+
+        int n = new java.util.Scanner(System.in).nextInt();
+
+        int nDigits = 1;
+
+        double div =  n;
+
+        while (div > 10) {
+
+            //System.out.println(div);
+
+            div = div / 10;
+            nDigits++;
+        }
+
+        System.out.println(n + " -> " + nDigits);
+
+
+    }
+}
