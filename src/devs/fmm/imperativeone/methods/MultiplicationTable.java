cd750f49292428b8096d4f9553cf8f0cb2142805
fmmdevs <fmm.devs@gmail.com>
2024-07-18 00:37:31 +0200

    Create a Multiplication Table

---
+++ src/devs/fmm/imperativeone/methods/MultiplicationTable.java
@@ -0,0 +1,29 @@
+package devs.fmm.imperativeone.methods;
+
+public class MultiplicationTable {
+
+    private static void multiplicationTableHtml(int quantity, String name1, int price1, String name2, int price2) {
+
+        System.out.println("<html>");
+
+        System.out.println("\t<table>");
+
+        System.out.println("\t\t<tr><th>QUANTITY</th><th>"+name1+"</th><th>"+name2+"</th></tr>");
+
+        for (int i = 1; i <= quantity; i++) {
+
+            System.out.println("\t\t<tr><td>" + i + "</td><td>" + (price1 * i) + "</td><td>" + (price2 * i) + "</td></tr>");
+
+        }
+
+        System.out.println("\t</table>");
+
+        System.out.println("</html>");
+    }
+
+    public static void main(String[] args) {
+
+        multiplicationTableHtml(10,"FLAMETOWER", 500,"FIRE EXTIGUISHER", 100);
+
+    }
+}
