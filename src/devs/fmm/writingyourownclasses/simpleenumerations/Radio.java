4949e66c296b7707d2ebb68b98acda5dd54eab6c
fmmdevs <fmm.devs@gmail.com>
2024-08-01 13:22:39 +0200

    added constants with frequency limits in Hz

--- src/devs/fmm/writingyourownclasses/simpleenumerations/Radio.java
+++ src/devs/fmm/writingyourownclasses/simpleenumerations/Radio.java
@@ -6,12 +6,31 @@ public class Radio {
     private double frequency;
     private Modulation modulation;
 
+    private final double MIN_AM_FREQUENCY = 148.5 * 1_000;
+    private final double MAX_AM_FREQUENCY = 26.1 * 1_000_000;
+    private final double MIN_FM_FREQUENCY = 87.5 * 1_000_000;
+    private final double MAX_FM_FREQUENCY = 108 * 1_000_000;
+
+
+    // String or double? units?
+    private double minFrequency;
+    private double maxFrequency;
+
+
     public Modulation getModulation() {
         return modulation;
     }
 
     public void setModulation(Modulation modulation) {
         this.modulation = modulation;
+        if (modulation == Modulation.AM) {
+            minFrequency = MIN_AM_FREQUENCY;
+            maxFrequency = MAX_AM_FREQUENCY;
+        }
+        if (modulation == Modulation.FM) {
+            minFrequency = MIN_FM_FREQUENCY;
+            maxFrequency = MAX_FM_FREQUENCY;
+        }
     }
 
     public int getVolume() {
@@ -54,6 +73,6 @@ public class Radio {
     @Override
     public String toString() {
 
-        return isOn() ? "Radio[volume=" + volume + ", frequency=" + frequency +" "+ modulation + "]" : "Radio[off]";
+        return isOn() ? "Radio[volume=" + volume + ", frequency=" + frequency + " " + modulation + "]" : "Radio[off]";
     }
 }
