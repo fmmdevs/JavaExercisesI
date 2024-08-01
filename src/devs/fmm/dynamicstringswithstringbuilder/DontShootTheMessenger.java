4854f7a8f47a900157871f80998ec7a450d54048
fmmdevs <fmm.devs@gmail.com>
2024-08-01 10:10:44 +0200

    Don't Shoot the Messenger

--- src/devs/fmm/dynamicstringswithstringbuilder/DontShootTheMessenger.java
+++ src/devs/fmm/dynamicstringswithstringbuilder/DontShootTheMessenger.java
@@ -2,38 +2,49 @@ package devs.fmm.dynamicstringswithstringbuilder;
 
 public class DontShootTheMessenger {
 
-    private static String joinSplitMessages(String... messengerPart) {
+    private static String joinSplitMessages(String... messengerParts) {
 
         StringBuilder assemblingMsg = new StringBuilder();
-/*
-        for (int i = 0; i < messengerPart.length; i++) {
-            assemblingMsg[i] = new StringBuilder(messengerPart[i]);
-        }*/
 
-        for (int i = 0; i < messengerPart.length; i++) {
-            if(messengerPart[i].length()>i){
-                System.out.println(messengerPart[i].charAt(i+1));
+        int maxLength = 0;
+
+        for (int i = 0; i < messengerParts.length; i++) {
+            if (messengerParts[i].length() > maxLength) {
+                maxLength = messengerParts[i].length();
             }
-
-            for (int j = 0; j < messengerPart[i].length(); j++) {
-                System.out.println("j="+j+" i="+i);
-                if(j<messengerPart.length) {
-                    assemblingMsg.append(messengerPart[j].charAt(i));
-                    System.out.println(assemblingMsg);
-                }
-            }
-
         }
 
+        //System.out.println(maxLength);
+        int length = 0;
+        // We use i to get the letter in that position, we need to know the maxLength of all messengerParts in order
+        // to get all the letters of each messengerPart and we are going to check the length of the current message
+        // because could be less than the maxLength
+        for (int i = 0; i < maxLength; i++) {
+            // We use j to iterate through the array of messengerParts
+            for (int j = 0; j < messengerParts.length; j++) {
+                // Obtaining the length of the current messengerPart
+                    length = messengerParts[j].length();
+                    //System.out.println("j "+j+" "+"i "+i+" length "+length);
+                    // If the current messagePart length is bigger than i we append that letter
+                    if(i<length){
+                        //System.out.print(messengerParts[j].charAt(i));
+                        assemblingMsg.append(messengerParts[j].charAt(i));
+                    }
 
-        return "";
+
+
+            }
+        }
+        System.out.println(assemblingMsg);
+
+
+        return assemblingMsg.toString();
     }
 
     public static void main(String[] args) {
         joinSplitMessages("Hoy", "ok");
-
-        /*joinSplitMessages("Hooky");
+        joinSplitMessages("Hooky");
         joinSplitMessages("Hk", "oy", "o");
-        joinSplitMessages("H", "", "ooky");*/
+        joinSplitMessages("H", "", "ooky");
     }
 }
