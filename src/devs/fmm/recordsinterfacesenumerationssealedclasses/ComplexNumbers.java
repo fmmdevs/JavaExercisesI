d147968af9532c1e5f5f1446c4630d8877d489ce
fmmdevs <fmm.devs@gmail.com>
2024-08-05 21:15:43 +0200

    Develop Record For Complex Numbers

--- src/devs/fmm/recordsinterfacesenumerationssealedclasses/ComplexNumbers.java
+++ src/devs/fmm/recordsinterfacesenumerationssealedclasses/ComplexNumbers.java
@@ -9,31 +9,52 @@ public class ComplexNumbers {
 
         @Override
         public String toString() {
-            return "Complex{" +
-                    "real=" + real +
-                    ", imaginary=" + imaginary +
-                    '}';
+
+            // String.format always rounding half-up
+            return "("+String.format("%.3f",real)+", "+ String.format("%.3f",imaginary)+"i)";
         }
 
         public  Complex add(Complex other){
+
             return new Complex(other.real+this.real, other.imaginary+this.imaginary);
         }
 
-        public Complex substract(Complex other){
-            return new Complex(other.real-this.real, other.real*this.imaginary+other.imaginary*this.real);
+        public Complex subtract(Complex other){
+            return new Complex(this.real-other.real, this.imaginary-other.imaginary);
         }
+
+
         public Complex multiply(Complex other){
-            return new Complex(other.real*this.real-this.imaginary*other.imaginary, other.imaginary*this.imaginary)
+            return new Complex(other.real*this.real-this.imaginary*other.imaginary, this.real*other.imaginary+this.imaginary*other.real);
         }
 
         // TO-DO abs()          !
 
+        public Complex abs(Complex other){
+            double real, imaginary;
+
+
+
+
+            return new Complex(Math.abs(this.real-other.real),Math.abs(this.imaginary-other.imaginary));
+        }
 
 
     }
 
-    public Complex add(Complex other){
+    public static void main(String[] args) {
+        Complex c1 = new Complex(-12,43);
+        Complex c2 = new Complex(2,-3);
+        Complex c3 = new Complex(-12,-10);
+
+        System.out.println(c1.abs(c2));
+        System.out.println(c2.abs(c1));
+        System.out.println(c1.abs(c3));
+        System.out.println(c3.abs(c1));
+        System.out.println(c1.subtract(c2));
+
 
     }
+
 
 }
