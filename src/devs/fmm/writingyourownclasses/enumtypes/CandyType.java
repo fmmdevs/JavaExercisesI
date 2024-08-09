99f045b67658bd5f4749bf913302896c6e39324c
fmmdevs <fmm.devs@gmail.com>
2024-08-09 20:08:58 +0200

    Deliver Random Candies

--- src/devs/fmm/writingyourownclasses/enumtypes/CandyType.java
+++ src/devs/fmm/writingyourownclasses/enumtypes/CandyType.java
@@ -2,6 +2,7 @@ package devs.fmm.writingyourownclasses.enumtypes;
 
 import java.util.Enumeration;
 import java.util.Optional;
+import java.util.Random;
 
 public enum CandyType {
     CARAMELS,
@@ -25,7 +26,22 @@ public enum CandyType {
             // Instead of null we use Optional.empty()
             return Optional.empty();
         }
+    }
 
+    static CandyType random(){
+        // We use Random to generate a int between 0 and the length of the array with the values of CandyType
+        Random random = new Random();
+        int randomInt = random.nextInt(CandyType.values().length);
+        return CandyType.values()[randomInt];
 
+    }
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i< 100; i++){
+
+            System.out.println(CandyType.random());
+
+        }
     }
 }
