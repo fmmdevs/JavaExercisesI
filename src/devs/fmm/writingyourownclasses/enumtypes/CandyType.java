4f963dc5e9048308da47cc55767a29d02ca526fa
fmmdevs <fmm.devs@gmail.com>
2024-08-10 20:13:46 +0200

    Tagging Candy with Addictive Value

--- src/devs/fmm/writingyourownclasses/enumtypes/CandyType.java
+++ src/devs/fmm/writingyourownclasses/enumtypes/CandyType.java
@@ -5,13 +5,33 @@ import java.util.Optional;
 import java.util.Random;
 
 public enum CandyType {
-    CARAMELS,
-    CHOCOLATE,
-    GUMMIES,
-    LICORICE,
-    LOLLIPOPS,
-    CHEWING_GUMS,
-    COTTON_CANDY;
+    CARAMELS ( 9 ),
+    CHOCOLATE ( 5 ),
+    GUMMIES ( 4 ),
+    LICORICE ( 3 ),
+    LOLLIPOPS ( 2 ),
+    CHEWING_GUMS ( 3 ),
+    COTTON_CANDY ( 1 );
+
+    private final int addictiveQuality;
+
+    CandyType(int addictiveQuality) {
+        this.addictiveQuality=addictiveQuality;
+    }
+
+    public int getAddictiveQuality(){
+        return addictiveQuality;
+    }
+
+    public CandyType next() {
+        return switch (this){
+            case GUMMIES -> CHOCOLATE;
+            case LOLLIPOPS -> Math.random()>0.5 ? LICORICE : CHEWING_GUMS;
+            case COTTON_CANDY -> LOLLIPOPS;
+            case LICORICE, CHEWING_GUMS -> GUMMIES;
+            case CARAMELS, CHOCOLATE -> CARAMELS;
+        };
+    }
 
     static Optional<CandyType> fromName(String input){
 
@@ -33,6 +53,10 @@ public enum CandyType {
         Random random = new Random();
         int randomInt = random.nextInt(CandyType.values().length);
         return CandyType.values()[randomInt];
+
+    }
+
+    private void addictiveQuality(){
 
     }
 
