9145ab260467afaf6db73abcc9f8e2724bcb02aa
fmmdevs <fmm.devs@gmail.com>
2024-08-18 19:01:46 +0200

    Write Current Date to File without Overwritting the Text in the File

--- src/devs/fmm/exceptionhandling/trywithresources/PrintWritingDate.java
+++ src/devs/fmm/exceptionhandling/trywithresources/PrintWritingDate.java
@@ -1,18 +1,20 @@
 package devs.fmm.exceptionhandling.trywithresources;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.time.LocalDateTime;
 
 public class PrintWritingDate {
     public static void main(String[] args) {
-        String filePath = "/user/workzone/Documentos/DevProjects/Java/Java Programming Exercises/Recursos/PrintWriter.txt";
-        try (PrintWriter writer = new PrintWriter(filePath);) {
+        String filePath = "/home/user/Documentos/DevProjects/Java/Java Programming Exercises/Recursos/PrintWriter.txt";
+        try (
+                FileWriter fileWriter = new FileWriter(filePath, true);
+                PrintWriter writer = new PrintWriter(fileWriter);) {
             String today = LocalDateTime.now().toString();
             System.out.println(today);
-            writer.write(today);
+            writer.write("\n"+today);
         } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e){
             System.out.println(e);
         }
     }
